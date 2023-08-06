53266b95b935a463a390534b4b6d26b6ecade56d
Abdul <abdur9718rahman@gmail.com>
2023-08-06 12:26:14 +0530

    implement all the features

---
+++ backend/Routes/CreateUser.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const jwtSecret = "MynameisEndToEndYouTubeChannel&#"
+router.post("/creatuser",[
+body('email', 'invalid value').isEmail(),
+  // password must be at least 5 chars long
+  body('name').isLength({ min: 5 }),
+  body('password', 'password should be aleast of 5 digits').isLength({ min: 5 })],
+
+ async (req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password, salt)
+
+  try {
+    await User.create({
+        name:req.body.name,
+        password:secPassword,
+        email:req.body.email,
+        location:req.body.location
+    })
+    res.json({success:true});
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:false});
+  }
+})
+
+
+router.post("/loginuser", [
+  body('email', 'invalid value').isEmail(),
+  // password must be at least 5 chars long
+  body('password', 'password should be aleast of 5 digits').isLength({ min: 5 })],
+  
+  async (req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  
+  let email = req.body.email;
+    try {
+    let userData = await User.findOne({email});
+
+    if(!userData){
+     return res.status(400).json({ errors: "Incorrect Email !" })
+    }
+  
+    const pwdCompare = bcrypt.compare(req.body.password, userData.password)
+
+    if(!pwdCompare){
+     return res.status(400).json({ errors: "Incorrect password !" })
+    }
+
+    const data = {
+      user:{
+        id:userData.id
+      }
+    }
+    const authToken = jwt.sign(data, jwtSecret)
+     return res.json({ success:true, authToken:authToken })
+
+    } catch (error) {
+      console.log(error)
+      res.json({success:false});
+    }
+  })
+
+module.exports = router;
